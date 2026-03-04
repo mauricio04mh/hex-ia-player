fc7f0880b8f5a9a8f44877752156ae3ec033b276
mharrymh <149264867+mharrymh@users.noreply.github.com>
2026-03-03 19:15:01 -0500

    First commit

---
+++ solution.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from player import Player
+from board import HexBoard
+
+from typing import List, Tuple
+
+
+Move = Tuple[int, int]
+
+
+class SmartPlayer(Player):
+    def __init__(self, player_id: int):
+        super().__init__(player_id)
+        self.opponent_id = 2 if player_id == 1 else 1
+
+    def play(self, board: HexBoard) -> Move:
+        pass
+
+    
